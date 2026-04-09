48c55a7c6ab33dfbf3ce69e58f47432f21f11162
KHUSHI RAWAT <khushi.23bce7131@vitapstudent.ac.in>
2026-04-09 22:57:10 +0530

    Initial commit

---
+++ netlify/functions/getTurtles.js
@@ -0,0 +1,24 @@
+const neo4j = require('neo4j-driver');
+
+exports.handler = async (event, context) => {
+  const driver = neo4j.driver(
+    process.env.NEO4J_URI,
+    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+  );
+  const session = driver.session();
+
+  try {
+    const result = await session.run('MATCH (t:Turtle) RETURN t');
+    const turtles = result.records.map(row => row.get('t').properties);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(turtles),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  } finally {
+    await session.close();
+    await driver.close();
+  }
+};
